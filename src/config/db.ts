7115c799f8802178ef1c0b2ccba2e98d3099d77b
arjun <arjunarunachalan@gmail.com>
2026-06-23 13:06:50 +0530

    cart bugg fixed

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 import { env } from "./env.js";
+import dns from "dns";
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 export async function connectDb() {
   mongoose.set("strictQuery", true);
